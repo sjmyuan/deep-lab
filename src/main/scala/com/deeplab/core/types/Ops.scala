8d79cc5d249f277cce3a7f80f4712f3903905b15
jiaming.shang <jiaming.shang@rea-group.com>
2017-04-16 18:56:43 +0800

    pass implicit ops to eval and grad

--- src/main/scala/com/deeplab/core/types/Ops.scala
+++ src/main/scala/com/deeplab/core/types/Ops.scala
@@ -6,43 +6,45 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException
   * Created by jiaming.shang on 4/14/17.
   */
 trait Ops[A] {
-  def +(lv:A,rv:A):A
-  def -(lv:A,rv:A):A
-  def *(lv:A,rv:A):A
-  def /(lv:A,rv:A):A
-  def exp(cap:A):A
-  def pow(lv:A,rv:A):A
-  def log(v:A):A
+  def +(lv: A, rv: A): A
+
+  def -(lv: A, rv: A): A
+
+  def *(lv: A, rv: A): A
+
+  def /(lv: A, rv: A): A
+
+  def exp(cap: A): A
+
+  def pow(lv: A, rv: A): A
+
+  def log(v: A): A
+
+  def zero(): A
+
+  def one(): A
+}
+
+class DoubleOps extends Ops[Double] {
+  override def +(lv: Double, rv: Double): Double = lv + rv
+
+  override def one(): Double = 1.0
+
+  override def /(lv: Double, rv: Double): Double = lv / rv
+
+  override def log(v: Double): Double = Math.log(v)
+
+  override def exp(cap: Double): Double = Math.exp(cap)
+
+  override def pow(lv: Double, rv: Double): Double = Math.pow(lv, rv)
+
+  override def -(lv: Double, rv: Double): Double = lv - rv
+
+  override def *(lv: Double, rv: Double): Double = lv * rv
+
+  override def zero(): Double = 0.0
 }
 
 object Ops {
-  def +[A](lv:A,rv:A):A ={
-    throw NotImplementedException
-  }
-  def -[A](lv:A,rv:A):A ={
-    throw NotImplementedException
-  }
-  def *[A](lv:A,rv:A):A ={
-    throw NotImplementedException
-  }
-  def /[A](lv:A,rv:A):A = {
-    throw NotImplementedException
-  }
-  def exp[A](cap:A):A ={
-    throw NotImplementedException
-  }
-  def pow[A](lv:A,rv:A):A={
-    throw NotImplementedException
-  }
-  def log[A](v:A):A={
-    throw NotImplementedException
-  }
-
-  def zero[A]():A ={
-    throw NotImplementedException
-  }
-
-  def one[A]():A={
-    throw NotImplementedException
-  }
+  implicit val doubleOps: Ops[Double] = new DoubleOps()
 }
--- src/main/scala/com/deeplab/core/types/SYMBOL.scala
+++ src/main/scala/com/deeplab/core/types/SYMBOL.scala
@@ -25,56 +25,56 @@ case class EXP[A](cap: SYMBOL[A]) extends SYMBOL[A]
 case class LOG[A](v: SYMBOL[A]) extends SYMBOL[A]
 
 object SYMBOL {
-  def eval[A](op: SYMBOL[A]): A => A = {
+  def eval[A](op: SYMBOL[A])(implicit ops: Ops[A]): A => A = {
     op match {
       case VAR(_) => { (x: A) => x }
       case CONS(_, v) => { (x: A) => v }
-      case ADD(lv, rv) => { x: A => Ops.+(eval(lv)(x), eval(rv)(x)) }
-      case SUB(lv, rv) => { x: A => Ops.-(eval(lv)(x), eval(rv)(x)) }
-      case MUL(lv, rv) => { x: A => Ops.*(eval(lv)(x), eval(rv)(x)) }
-      case DIV(lv, rv) => { x: A => Ops./(eval(lv)(x), eval(rv)(x)) }
-      case POW(v, cap) => { x: A => Ops.pow(eval(v)(x), eval(cap)(x)) }
-      case EXP(cap) => { x: A => Ops.exp(eval(cap)(x)) }
-      case LOG(v) => { x: A => Ops.log(eval(v)(x)) }
+      case ADD(lv, rv) => { x: A => ops.+(eval(lv)(ops)(x), eval(rv)(ops)(x)) }
+      case SUB(lv, rv) => { x: A => ops.-(eval(lv)(ops)(x), eval(rv)(ops)(x)) }
+      case MUL(lv, rv) => { x: A => ops.*(eval(lv)(ops)(x), eval(rv)(ops)(x)) }
+      case DIV(lv, rv) => { x: A => ops./(eval(lv)(ops)(x), eval(rv)(ops)(x)) }
+      case POW(v, cap) => { x: A => ops.pow(eval(v)(ops)(x), eval(cap)(ops)(x)) }
+      case EXP(cap) => { x: A => ops.exp(eval(cap)(ops)(x)) }
+      case LOG(v) => { x: A => ops.log(eval(v)(ops)(x)) }
     }
   }
 
-  def grad[A](op: SYMBOL[A], name: String): A => A = {
+  def grad[A](op: SYMBOL[A], name: String)(implicit ops: Ops[A]): A => A = {
     op match {
-      case VAR(vn) => if (vn == name) { x: A => Ops.one() } else { x: A => Ops.zero() }
-      case CONS(vn, v) => if (vn == name) { x: A => Ops.one() } else { x: A => Ops.zero() }
-      case ADD(lv, rv) => { x: A => Ops.+(grad(lv, name)(x), grad(rv, name)(x)) }
-      case SUB(lv, rv) => { x: A => Ops.-(grad(lv, name)(x), grad(rv, name)(x)) }
+      case VAR(vn) => if (vn == name) { x: A => ops.one() } else { x: A => ops.zero() }
+      case CONS(vn, v) => if (vn == name) { x: A => ops.one() } else { x: A => ops.zero() }
+      case ADD(lv, rv) => { x: A => ops.+(grad(lv, name)(ops)(x), grad(rv, name)(ops)(x)) }
+      case SUB(lv, rv) => { x: A => ops.-(grad(lv, name)(ops)(x), grad(rv, name)(ops)(x)) }
       case MUL(lv, rv) => {
         x: A =>
-          Ops.+(Ops.*(grad(lv, name)(x), eval(rv)(x)), Ops.*(eval(lv)(x), grad(rv, name)(x)))
+          ops.+(ops.*(grad(lv, name)(ops)(x), eval(rv)(ops)(x)), ops.*(eval(lv)(ops)(x), grad(rv, name)(ops)(x)))
       }
       case DIV(lv, rv) => {
         x: A => {
-          val ldf = grad(lv, name)(x)
-          val rdf = grad(rv, name)(x)
-          val lf = eval(lv)(x)
-          val rf = eval(rv)(x)
-          Ops.-(Ops./(ldf, rf), Ops./(lf, Ops.*(Ops.pow(rf, Ops.+(Ops.one(), Ops.one())), rdf)))
+          val ldf = grad(lv, name)(ops)(x)
+          val rdf = grad(rv, name)(ops)(x)
+          val lf = eval(lv)(ops)(x)
+          val rf = eval(rv)(ops)(x)
+          ops.-(ops./(ldf, rf), ops./(lf, ops.*(ops.pow(rf, ops.+(ops.one(), ops.one())), rdf)))
         }
       }
       case POW(v, cap) => {
         x: A => {
-          val capf = eval(cap)(x)
-          val vf = eval(v)(x)
-          val capdf = grad(cap, name)(x)
-          val vdf = grad(v, name)(x)
-          Ops.+(Ops.*(Ops.*(capf, Ops.pow(vf, Ops.-(capf, Ops.one))), vdf), Ops.*(Ops.*(Ops.pow(vf, capf), Ops.log(vf)), capdf))
+          val capf = eval(cap)(ops)(x)
+          val vf = eval(v)(ops)(x)
+          val capdf = grad(cap, name)(ops)(x)
+          val vdf = grad(v, name)(ops)(x)
+          ops.+(ops.*(ops.*(capf, ops.pow(vf, ops.-(capf, ops.one))), vdf), ops.*(ops.*(ops.pow(vf, capf), ops.log(vf)), capdf))
         }
       }
       case EXP(cap) => {
         x: A => {
-          Ops.*(Ops.exp(eval(cap)(x)), grad(cap, name)(x))
+          ops.*(ops.exp(eval(cap)(ops)(x)), grad(cap, name)(ops)(x))
         }
       }
       case LOG(v) => {
         x: A => {
-          Ops.*(Ops./(Ops.one(), eval(v)(x)), grad(v, name)(x))
+          ops.*(ops./(ops.one(), eval(v)(ops)(x)), grad(v, name)(ops)(x))
         }
       }
     }
